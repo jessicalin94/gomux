d71611875f934ff06bad19e5cd731d746fca35ad
Jessica Lin <66900456+jessicalin94@users.noreply.github.com>
2023-11-29 23:16:24 +1100

    create multiplexer and add handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		s := Status{Status: "ok"}
+		json.NewEncoder(w).Encode(s)
+	})
+
 	server := &http.Server{
 		ReadHeaderTimeout: 30 * time.Second,
 		Addr:              ":8080",
-		Handler:           mux,
+		Handler:           mux, // "handler" does that mean this could be just a single handler ie non-multiplexer
 	}
 
 	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		panic(err)
 	}
+}
+
+type Status struct {
+	Status string `json:"status"`
 }
